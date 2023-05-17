3be2ea91b972d3a3abff5c0e37027a09285f521e
Paul Salaets <psalaets@gmail.com>
2023-05-16 21:54:35 -0500

    Restructure mouse handling

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,22 +1,13 @@
 import { splitTextNodes } from "split-text-nodes";
 
-activate(document);
-
-/**
- * @param {Document} document
- */
-function activate(document) {
-  const infoCard = createInfoCard();
-
-  function createInfoCard() {
-    const cardElement = document.createElement('div');
-    cardElement.style.cssText = `
+function createInfoCard() {
+  const cardElement = document.createElement('div');
+  cardElement.style.cssText = `
 color: #111;
 background-color: #fff;
 border: 2px solid #111;
 border-radius: 0.5rem;
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-display: none;
 font-size: 1rem;
 font-family: Consolas, monaco, monospace;
 padding: 0.8rem;
@@ -25,93 +16,92 @@ z-index: 1;
 max-width: 20rem;
 `;
 
-    document.body.appendChild(cardElement);
+  document.body.appendChild(cardElement);
 
-    return {
-      hide() {
-        cardElement.style.display = 'none';
-      },
-      show() {
-        cardElement.style.display = 'block';
-      },
-      /**
-       * @param {number} viewportX
-       * @param {number} viewportY
-       */
-      moveTo(viewportX, viewportY) {
-        cardElement.style.left = `${viewportX}px`;
+  return {
+    /**
+     * @param {number} viewportX
+     * @param {number} viewportY
+     */
+    moveTo(viewportX, viewportY) {
+      cardElement.style.left = `${viewportX}px`;
 
-        const yOffset = 10;
-        cardElement.style.top = `${viewportY + yOffset}px`;
-      },
-      clear() {
-        cardElement.innerHTML = '';
-      },
-      /**
-       * @param {HTMLElement} element - The element we're showing stats for
-       * @param {Stats} stats
-       */
-      update(element, stats) {
-        cardElement.innerHTML = `
+      const yOffset = 10;
+      cardElement.style.top = `${viewportY + yOffset}px`;
+    },
+    /**
+     * @param {HTMLElement} element - The element we're showing stats for
+     * @param {Stats} stats
+     */
+    update(element, stats) {
+      cardElement.innerHTML = `
 <div style="overflow-x: clip;white-space: nowrap;text-overflow: ellipsis;padding-bottom: 0.8rem; margin-bottom: 0.8rem; border-bottom: 1px solid lightgray;">${elementInfo(element)}</div>
 <div>Median: ${stats.median}</div>
 <div>Max: ${stats.max}</div>
 <div style="color: dimgray; font-size: 0.8rem; margin-top: 0.8rem; text-align: center;">Esc to close</div>
 `;
-      },
-      destroy() {
-        cardElement.remove();
-      },
-    };
-  }
 
-  /**
-   * @param {HTMLElement} element
-   * @returns {string} html
-   */
-  function elementInfo(element) {
-    return [
-      '<span>',
-      `<span style="color: #5E2CA5;">${element.nodeName.toLowerCase()}</span>`,
-      element.id ? `<span style="color: #137752">#${element.id}</span>` : '',
-      element.classList.length ? `<span style="color: #E7040F;">.${Array.from(element.classList).join('.')}</span>` : '',
-      '</span>',
-    ].join('');
-  }
+      /**
+       * @param {HTMLElement} element
+       * @returns {string} html
+       */
+      function elementInfo(element) {
+        return [
+          '<span>',
+          `<span style="color: #5E2CA5;">${element.nodeName.toLowerCase()}</span>`,
+          element.id ? `<span style="color: #137752">#${element.id}</span>` : '',
+          element.classList.length ? `<span style="color: #E7040F;">.${Array.from(element.classList).join('.')}</span>` : '',
+          '</span>',
+        ].join('');
+      }
+    },
+    destroy() {
+      cardElement.remove();
+    },
+  };
+}
 
-  /**
-   * @param {MouseEvent} event
-   */
-  function onMouseOver(event) {
+let infoCard = null;
+
+trackMouse({
+  onMouseOver(event) {
     const target = event.target;
 
     if (target instanceof HTMLElement) {
       const stats = computeStats(lineLengths(target));
       if (stats) {
+        infoCard = createInfoCard();
         infoCard.update(target, stats);
         infoCard.moveTo(event.clientX, event.clientY);
-        infoCard.show();
       }
     }
+  },
+  onMouseOut() {
+    if (infoCard) {
+      infoCard.destroy();
+    }
+    infoCard = null;
+  },
+  onMouseMove(event) {
+    if (infoCard) {
+      infoCard.moveTo(event.clientX, event.clientY);
+    }
+  },
+  onTearDown() {
+    if (infoCard) {
+      infoCard.destroy();
+    }
+    infoCard = null;
   }
+});
 
-  function onMouseOut() {
-    infoCard.hide();
-  }
-
-  /**
-   * @param {MouseEvent} event
-   */
-  function onMouseMove(event) {
-    infoCard.moveTo(event.clientX, event.clientY);
-  }
-
+function trackMouse({onMouseOver, onMouseOut, onMouseMove, onTearDown}) {
   /**
    * @param {KeyboardEvent} event
-   */
-  function onKeyDown(event) {
-    if (event.key === 'Escape') {
-      tearDown();
+  */
+ function onKeyDown(event) {
+   if (event.key === 'Escape') {
+     tearDown();
     }
   }
 
@@ -126,10 +116,10 @@ max-width: 20rem;
     document.removeEventListener('mousemove', onMouseMove);
     document.removeEventListener('keydown', onKeyDown);
 
-    infoCard.destroy();
+    onTearDown && onTearDown();
   }
 
-  return () => tearDown();
+  return tearDown;
 }
 
 /**
